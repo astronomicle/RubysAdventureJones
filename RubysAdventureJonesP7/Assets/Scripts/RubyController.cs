24b43470fc5c9c29ea65befb2e7c121d9abd1c8b
astronomicle <116121600+astronomicle@users.noreply.github.com>
2022-11-01 15:29:07 -0700

    Added to RubyController Script
    Added the vertical directs for ruby to move

--- RubysAdventureJonesP7/Assets/Scripts/RubyController.cs
+++ RubysAdventureJonesP7/Assets/Scripts/RubyController.cs
@@ -14,9 +14,13 @@ public class RubyController : MonoBehaviour
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
+        float verticle = Input.GetAxis("Verticle");
+
         Debug.Log(horizontal);
+        Debug.Log(verticle);
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+        position.x = position.x + 0.1f * horizontal;
+        position.y = position.y + 0.1f * verticle;
         transform.position = position;
     }
 }
