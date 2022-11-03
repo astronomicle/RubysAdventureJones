e0238da0e40d32695adff846fd786b4de7b7a986
astronomicle <116121600+astronomicle@users.noreply.github.com>
2022-11-03 15:27:13 -0700

    Finished Ruby's control and added tiles
    I finished the script for controlling Ruby then added and sliced all the tiles there were

--- RubysAdventureJonesP7/Assets/Scripts/RubyController.cs
+++ RubysAdventureJonesP7/Assets/Scripts/RubyController.cs
@@ -7,20 +7,17 @@ public class RubyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
-        float verticle = Input.GetAxis("Verticle");
-
-        Debug.Log(horizontal);
-        Debug.Log(verticle);
+        float vertical = Input.GetAxis("Vertical");
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f * horizontal;
-        position.y = position.y + 0.1f * verticle;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
         transform.position = position;
     }
 }
