595f349191708901ed69ea28e0d72657f53b9255
astronomicle <116121600+astronomicle@users.noreply.github.com>
2022-11-18 15:27:47 -0800

    Added prefabs and rigid bodies
    I added prefabs for most of the game objects I'm gonna use and added colliders for the metal box and Ruby

--- RubysAdventureJonesP7/Assets/Scripts/RubyController.cs
+++ RubysAdventureJonesP7/Assets/Scripts/RubyController.cs
@@ -4,20 +4,26 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+
     void Start()
     {
-
+        rigidbody2d = GetComponent < Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        Vector2 position = transform.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
-        transform.position = position;
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + 3f * horizontal * Time.deltaTime;
+        position.y = position.y + 3f * vertical * Time.deltaTime;
+
+        rigidbody2d.MovePosition(position);
     }
 }
